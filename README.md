750ce51666f1e0ff51f8ebc77856b75208fa20a0
Edwin Westerhoud <edwinwesterhoud@voormedia.com>
2018-11-21 13:33:56 +0100

    Initial commit.

---
+++ README.md
@@ -0,0 +1,136 @@
+# Introduction
+
+Metalsmith plugin that creates multiple localised branches of your static site and
+provides helpers for translations.
+
+The plugin does not rely on any i18n package. Instead it requires to specify an
+i18n instance as param. This has been tested with [i18next](https://www.i18next.com).
+
+# Quick start
+
+## Installation
+
+    npm i --save metalsmith-i18n
+
+
+## Example
+
+Given the following code and input folder structure, here is the output structure.
+
+```js
+Metalsmith(__dirname)
+	.use(i18next(
+		pattern: '**/*.hbs',
+		defaultLocale: 'en',
+		alternativeLocales: ['nl'],
+		i18n: i18next
+	))
+	.build(function(err, files){
+		if (err) console.error(err.stack)
+	})
+
+Input                                                   Output
+-----                                                   ------
+.                                                       .
+|                                                       |
++- index.js                                             +- en
+|                                                       |   |
++- locales                                              |   +-- index.hamlc
+|   |                                                   |
+|   +-- en                                              +- nl
+|   |    |                                              |   |
+|   |    +-- public.json                                |   +-- index.hamlc
+|   |                                                   |
+|   +-- nl                                              +- images
+|        |                                                  |
+|        +-- public.json                                    +-- smile.png
+|
++- src
+    |
+    +-- index.hamlc
+    |
+    +-- images
+         |
+         +-- smile.png
+```
+
+## i18n Helper Functions
+
+Once initialized, all files that match the pattern are given three helper function accessible from
+templates and downstream functions:
+
+#### `function t(String key, [Object options])`
+
+translates the key. The key and options are passed on to the i18n instance. The
+target locale is added to the options.
+
+Example with i18next:
+Given the default options and namespaces `['common','public']`
+```
+t('public:home.title')		// => Looks in ./locales/<locale>/public.json for home['title']
+t('home.title')		        // => Looks in ./locales/<locale>/common.json for home['title']
+t('title')                // => Looks in ./locales/<locale>/common.json for title
+```
+<hr>
+
+#### `function tpath(String file)`
+translates the path of a file without locale information to one with
+
+Examples:
+```
+// Given the default path option of ':locale/:file'
+tpath('/signup.html')            // => /<locale>/signup.html
+tpath('/products/widget.html')   // => /<locale>/products/widget.html
+
+// Given a path option of ':dir/:name-:locale:ext'
+tpath('/signup.html')            // => /signup-<locale>.html
+tpath('/products/widget.html')   // => /products/widget--<locale>.html
+
+```
+<hr>
+
+#### Interpolation with i18next
+You can use variables in the locale files:
+
+```
+{
+	"hello": "Hello {{name}}"
+}
+```
+
+then pass variable values to the helper:
+
+(hamlc)
+```
+= @tt('hello', {"name": 'John Doe'})
+```
+
+(handlebars)
+```
+{{{t "hello" name="John Doe"}}}
+```
+
+
+# License
+
+The MIT License (MIT)
+
+Copyright (c) 2015 Eric Methot
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
---
+++ test.js
@@ -0,0 +1,321 @@
+// 'use strict'
+
+// var chai       = require('chai'),
+// 	should     = chai.should(),
+// 	Metalsmith = require('metalsmith'),
+// 	i18next    = require('i18next'),
+// 	i18nextMS  = require('.'),
+// 	helpers    = require('./lib/helpers'),
+// 	copy       = require('metalsmith-copy'),
+// 	templates  = require('metalsmith-in-place'),
+// 	uglify     = require('metalsmith-uglify')
+
+// describe('metalsmith-i18next', function(){
+
+
+// 	// ------------------------------------------------------------------------
+// 	// Generic Test Case
+// 	// ------------------------------------------------------------------------
+
+// 	function metalsmithTest(config, check) {
+
+// 		var once = false
+
+// 		return function(done) {
+// 			Metalsmith('./examples')
+// 			.use(i18nextMS(config))
+// 			.use(templates({
+// 				engine: 'haml-coffee',
+// 				pattern:  '**/*.hamlc'
+// 			}))
+// 			.use(copy({
+// 				pattern: '**/*.hamlc',
+// 				extension: '.txt',
+// 				move: true
+// 			}))
+// 			.use(uglify())
+// 			.build(function(err, files){
+// 				if (once) return
+// 				once = true
+// 				if (err) return done(err)
+// 				try {
+// 					check(files)
+// 					done()
+// 				} catch(err) {
+// 					done(err)
+// 				}
+// 			})
+// 		}
+// 	}
+
+// 	function prop(value) {
+// 		return function(v) {
+// 			return arguments.length? (value = v) : value
+// 		}
+// 	}
+
+
+// 	// ------------------------------------------------------------------------
+// 	// Helper Test Cases
+// 	// ------------------------------------------------------------------------
+
+// 	it('should return the expected file parts', function(done){
+
+// 		i18next.init({
+// 			lng: 'en',
+// 			resGetPath: './examples/locales/__lng__/__ns__.json',
+// 			ns: {namespaces:['translations'], defaultNs:'translations'},
+// 			preload: ['en','fr'],
+// 			getAsync: false,
+// 			fallbackLng: false
+// 		})
+
+// 		var fileParts = helpers(i18next).fileParts,
+// 			parts
+
+// 		fileParts('index.html').should.eql({
+// 			file:   'index.html',
+//         	ext:    '.html',
+//         	base:   'index.html',
+//         	dir:    '',
+//         	name:   'index',
+//         	locale: 'en',
+//         	hash:   '',
+//         	query:  ''
+// 		})
+
+// 		fileParts('a/b/index.php?filter=cars').should.eql({
+// 			file:   'a/b/index.php?filter=cars',
+//         	ext:    '.php',
+//         	base:   'index.php',
+//         	dir:    'a/b',
+//         	name:   'index',
+//         	locale: 'en',
+//         	hash:   '',
+//         	query:  '?filter=cars'
+// 		})
+
+// 		fileParts('computers/laptop.html#specs','fr').should.eql({
+// 			file:   'computers/laptop.html#specs',
+//         	ext:    '.html',
+//         	base:   'laptop.html',
+//         	dir:    'computers',
+//         	name:   'laptop',
+//         	locale: 'fr',
+//         	hash:   '#specs',
+//         	query:  ''
+// 		})
+
+// 		fileParts('a/b/index.php?filter=cars#heading','fr').should.eql({
+// 			file:   'a/b/index.php?filter=cars#heading',
+//         	ext:    '.php',
+//         	base:   'index.php',
+//         	dir:    'a/b',
+//         	name:   'index',
+//         	locale: 'fr',
+//         	hash:   '#heading',
+//         	query:  '?filter=cars'
+// 		})
+
+// 		done()
+// 	})
+
+// 	it('should localize the path as expected', function(done){
+
+// 		var path   = prop(':locale/:file'),
+// 			locale = prop('en'),
+// 			tpath  = helpers(i18next, {path, locale}).tpath
+
+// 		tpath('/').should.equal('/en')
+// 		tpath('/index.html').should.equal('/en/index.html')
+// 		tpath('/index.html','fr').should.equal('/fr/index.html')
+
+// 		path(':dir/:name-:locale:ext:query:hash')
+// 		tpath('/foo/bar.php?filter=cars#heading').should.equal('/foo/bar-en.php?filter=cars#heading')
+
+// 		locale('fr')
+// 		tpath('/foo/bar.php?filter=cars#heading').should.equal('/foo/bar-fr.php?filter=cars#heading')
+
+// 		done()
+// 	})
+
+// 	it('should translate as expected', function(done){
+
+// 		var path      = prop(':locale/:file'),
+// 			locale    = prop('fr'),
+// 			prefix    = prop(['home','common']),
+// 			namespace = prop('translations'),
+// 			fn        = helpers(i18next, {path, prefix, locale, namespace})
+
+// 		fn.t('translations:home.hello',{lng:'fr'}).should.equal('Bonjour __name__')
+// 		fn.tt('hello', {"name": "John Doe"}).should.equal('Bonjour John Doe')
+// 		fn.tt('foo').should.equal('Fou!!!')
+// 		fn.tt('bar').should.equal('[home,common].bar')
+// 		done()
+// 	})
+
+
+// 	// ------------------------------------------------------------------------
+// 	// Normal Test Cases
+// 	// ------------------------------------------------------------------------
+
+// 	it('should create two localised directories each with index.txt', metalsmithTest(
+// 		{
+// 			pattern: '**/*.hamlc',
+// 			locales: ['en','fr'],
+// 			nsPath: './examples/locales/__lng__/__ns__.json',
+// 			namespaces: ['translations']
+// 		},
+// 		function(files) {
+
+// 			var enFile = files['en/index.txt'],
+// 				frFile = files['fr/index.txt']
+
+// 			should.exist(enFile)
+// 			should.exist(frFile)
+
+// 			enFile.contents.toString('utf8').should.equal('Hello John Doe')
+// 			frFile.contents.toString('utf8').should.equal('Bonjour John Doe')
+
+// 			should.exist(enFile.i18nBootstrap)
+// 			should.exist(frFile.i18nBootstrap)
+
+// 			should.exist(enFile.i18nOrigPath)
+// 			should.exist(frFile.i18nOrigPath)
+
+// 			should.exist(enFile.i18nResStore)
+// 			should.exist(frFile.i18nResStore)
+
+// 			enFile.i18nResStore.should.eql({en: {translations: {common:{foo:'Foo!!!'}, home: {hello: 'Hello __name__'}},foo:{foo:{bar:'Foobar'}}}})
+// 			frFile.i18nResStore.should.eql({fr: {translations: {common:{foo:'Fou!!!'}, home: {hello: 'Bonjour __name__'}},foo:{foo:{bar:'Foobar!!'}}}})
+// 		}
+// 	))
+
+// 	it('should omit locale for fallback locale', metalsmithTest(
+// 		{
+// 			pattern: '**/*.hamlc',
+// 			locales: ['en','fr'],
+// 			nsPath: './examples/locales/__lng__/__ns__.json',
+// 			namespaces: ['translations'],
+// 			fallbackLng: 'en',
+// 			omitFallbackLng: true
+// 		},
+// 		function(files) {
+
+// 			var enFile = files['index.txt'],
+// 				frFile = files['fr/index.txt']
+
+// 			should.exist(enFile)
+// 			should.exist(frFile)
+
+// 			enFile.contents.toString('utf8').should.equal('Hello John Doe')
+// 			frFile.contents.toString('utf8').should.equal('Bonjour John Doe')
+
+// 			should.exist(enFile.i18nBootstrap)
+// 			should.exist(frFile.i18nBootstrap)
+
+// 			should.exist(enFile.i18nOrigPath)
+// 			should.exist(frFile.i18nOrigPath)
+
+// 			should.exist(enFile.i18nResStore)
+// 			should.exist(frFile.i18nResStore)
+
+// 			enFile.i18nResStore.should.eql({en: {translations: {common:{foo:'Foo!!!'}, home: {hello: 'Hello __name__'}},foo:{foo:{bar:'Foobar'}}}})
+// 			frFile.i18nResStore.should.eql({fr: {translations: {common:{foo:'Fou!!!'}, home: {hello: 'Bonjour __name__'}},foo:{foo:{bar:'Foobar!!'}}}})
+// 		}
+// 	))
+
+
+// 	it('should create both index-en.txt and index-fr.txt in the same directory', metalsmithTest(
+// 		{
+// 			pattern: '**/*.hamlc',
+// 			locales: ['en','fr'],
+// 			nsPath: './examples/locales/__lng__/__ns__.json',
+// 			namespaces: ['translations'],
+// 			path: ':dir/:name-:locale:ext'
+// 		},
+// 		function(files) {
+
+// 			var enFile = files['index-en.txt'],
+// 				frFile = files['index-fr.txt']
+
+// 			should.exist(enFile)
+// 			should.exist(frFile)
+
+// 			enFile.contents.toString('utf8').should.equal('Hello John Doe')
+// 			frFile.contents.toString('utf8').should.equal('Bonjour John Doe')
+// 		}
+// 	))
+
+// 	it('file should have t, tt, tpath and locale', metalsmithTest(
+// 		{
+// 			pattern: '**/*.hamlc',
+// 			locales: ['en','fr'],
+// 			nsPath: './examples/locales/__lng__/__ns__.json',
+// 			namespaces: ['translations'],
+// 			path: ':dir/:name-:locale:ext'
+// 		},
+// 		function(files) {
+
+// 			var enFile = files['index-en.txt'],
+// 				frFile = files['index-fr.txt']
+
+// 			should.exist(enFile)
+// 			should.exist(frFile)
+
+// 			enFile.t.should.exist
+// 			enFile.tt.should.exist
+// 			enFile.tpath.should.exist
+// 			enFile.locale.should.equal('en')
+
+// 			frFile.t.should.exist
+// 			frFile.tt.should.exist
+// 			frFile.tpath.should.exist
+// 			frFile.locale.should.equal('fr')
+// 		}
+// 	))
+
+
+// 	it('should allow tpath to override the locale', metalsmithTest(
+// 		{
+// 			pattern: '**/*.hamlc',
+// 			locales: ['en','fr'],
+// 			nsPath: './examples/locales/__lng__/__ns__.json',
+// 			namespaces: ['translations']
+// 		},
+// 		function(files) {
+
+// 			var enFile = files['en/index.txt']
+
+// 			should.exist(enFile)
+
+// 			enFile.tpath('/toto.txt').should.equal('/en/toto.txt')
+// 			enFile.tpath('/toto.txt','fr').should.equal('/fr/toto.txt')
+// 		}
+// 	))
+
+// 	it('should allow tpath to omit fallback locale', metalsmithTest(
+// 		{
+// 			pattern: '**/*.hamlc',
+// 			locales: ['en','fr'],
+// 			nsPath: './examples/locales/__lng__/__ns__.json',
+// 			namespaces: ['translations'],
+// 			fallbackLng: 'en',
+// 			omitFallbackLng: true
+// 		},
+// 		function(files) {
+
+// 			var enFile = files['index.txt']
+// 			var frFile = files['fr/index.txt']
+
+// 			should.exist(enFile)
+// 			should.exist(frFile)
+
+// 			enFile.tpath('/toto.txt').should.equal('/toto.txt')
+// 			enFile.tpath('/toto.txt','fr').should.equal('/fr/toto.txt')
+
+// 			frFile.tpath('/toto.txt').should.equal('/fr/toto.txt')
+// 			frFile.tpath('/toto.txt','en').should.equal('/toto.txt')
+// 		}
+// 	))
+// })
